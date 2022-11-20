dbcd687656a2b5dea5a530a7032696d3204e8b5d
BOMEN <bomen21x@gmail.com>
2022-11-21 01:17:08 +0700

    Update __manifest__.py

--- ctp_customize_title/__manifest__.py
+++ ctp_customize_title/__manifest__.py
@@ -7,36 +7,38 @@
 ###################################################################################
 
 {
-    'name': 'Customize Web Backend Title',
-    'version': '15.0.1.0.1',
-    'summary': """ 
+    "name": "Customize Web Backend Title",
+    "version": "15.0.1.0.1",
+    "summary": """ 
             Cybernetics Plus Tools Customize Web Backend Title
             .""",
-    'description': """ 
+    "description": """ 
             Cybernetics Plus Tools Customize Web Backend Title
             .""",
-    'author': 'Cybernetics Plus Co., Ltd.',
-    'website': 'https://www.cybernetics.plus',
-    'live_test_url': 'https://www.cybernetics.plus',
-    'images': ['static/description/banner.png'],
-    'category': 'Tools',
-    'license': 'LGPL-3',
-    'installable': True,
-    'application': True,
-    'auto_install': False,
-    'contributors': [
-        'Developer <dev@cybernetics.plus>',
+    "author": "Cybernetics Plus",
+    "company": "Cybernetics Plus Co., Ltd.",
+    "maintainer": "Cybernetics Plus Co., Ltd.",
+    "website": "https://www.cybernetics.plus",
+    "live_test_url": "https://www.cybernetics.plus",
+    "images": ["static/description/banner.png"],
+    "category": "Tools",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+    "contributors": [
+        "C+ Developer <dev@cybernetics.plus>",
     ],
-    'depends': ['base_setup'],
-    'demo': [
-        'data/title_config.xml',
+    "depends": ["base_setup"],
+    "demo": [
+        "data/title_config.xml",
     ],
-    'data': [
-        'views/res_config.xml',
+    "data": [
+        "views/res_config.xml",
     ],
-    'assets': {
-        'web.assets_backend': [
-            'ctp_customize_title/static/src/js/customize_title.js',
+    "assets": {
+        "web.assets_backend": [
+            "ctp_customize_title/static/src/js/customize_title.js",
         ],
     },
 }
